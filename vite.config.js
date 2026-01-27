a916ec7ab4438f6d38df7be9b6528b8d111bb93d
KIM <wips6123@gmail.com>
2026-01-27 14:35:48 +0900

    fix multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
     port: 5173,
     open: true,
     host: true,
+    fs: {
+      allow: ["C:/Users/ean/OneDrive/바탕 화면/WIPS", "C:/WIPS"],
+    },
   },
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        search: resolve(__dirname, "search.html"),
+        billing: resolve(__dirname, "billing.html"),
+        nation: resolve(__dirname, "nation.html"),
+      },
+    },
   },
 });
